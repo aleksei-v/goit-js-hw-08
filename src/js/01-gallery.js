598950f0e47010da3c4a8c1a223585ee8f8aa8d6
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-07-28 12:11:14 +0300

    update first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+const makeImageGallery = document.querySelector(".gallery");
+
+function createLi(images) {
+    return images.reduce((acc, { preview, description, original }) => acc + `
+  <a class="gallery__item" 
+  href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}"
+    </a> `
+  , "")
+}
+const result = createLi(galleryItems);
+
+makeImageGallery.insertAdjacentHTML('beforeend', result);
+
+
+function handleClick(evt) {
+  evt.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captionsData: 'alt',
+        captionPosition: "top",
+    });
+}   
+makeImageGallery.addEventListener('click', handleClick);
