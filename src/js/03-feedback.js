eaf66cdeffdfc7525e5166da07139341d12266bb
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-07-31 16:23:57 +0300

    add solution of task 2 and update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEYS = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    email: document.querySelector(".feedback-form input"),
+    message: document.querySelector(".feedback-form textarea"),
+}
+const savedData = {};
+
+populateData();
+
+const onFormInputs = (evt) => {
+    
+    savedData[evt.target.name] = evt.target.value;
+
+    localStorage.setItem(STORAGE_KEYS, JSON.stringify(savedData));
+
+}
+function populateData() {
+
+    const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEYS));
+    console.log(parsedData);
+
+
+    if (parsedData) {
+        refs.message.value = parsedData.message || '';
+        refs.email.value = parsedData.email || '';
+    }
+}
+
+refs.form.addEventListener('input', onFormInputs)
+
 
